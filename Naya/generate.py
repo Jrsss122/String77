4a58430cf07c2f1de172064088c4444918a1718c
J Robott <115394912+Jrsss122@users.noreply.github.com>
2023-12-11 04:18:20 +0700

    Update generate.py

--- Naya/generate.py
+++ Naya/generate.py
@@ -143,7 +143,7 @@ async def generate_session(bot: Client, msg: Message, telethon=False, is_bot: bo
         string_session = client.session.save()
     else:
         string_session = await client.export_session_string()
-    text = f"**{ty.upper()} NIH JING.** \n\n`{string_session}` \n\n**Minimal Bilang Makasih Ke** @disinikazu **Atau Ke** @kazusupportgrp **Karna Akun Lu Kaga Deak**"
+    text = f"**{ty.upper()} NIH JING.** \n\n`{string_session}` \n\n**Minimal Bilang Makasih Ke** @Jrsss777 **Karna Akun Lu Kaga Deak**"
     try:
         try:
             if telethon:
